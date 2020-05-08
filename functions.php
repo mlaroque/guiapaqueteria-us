81c7cf502b41c583ef8a324245a49534099482dd
root <root@server.lacomuna.online>
2020-05-08 00:00:28 -0400

    daily backup Fri May  8 00:00:28 EDT 2020

--- functions.php
+++ functions.php
@@ -183,6 +183,8 @@ function lacomuna_theme_scripts() {
 
 	wp_enqueue_script( 'lacomuna-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+    wp_enqueue_script( 'nav-walker', get_template_directory_uri() . '/js/nav-walker.js', array(), '', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
