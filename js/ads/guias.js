f10c2e02cca2a725d11e20b4a2c9e9b10564b82f
root <root@server.lacomuna.online>
2020-12-11 00:00:29 -0500

    daily backup Fri Dec 11 00:00:29 EST 2020

--- js/ads/guias.js
+++ js/ads/guias.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
 	    lazyAds.forEach(ad => {
 	        if (ad.offsetTop < window.innerHeight + window.pageYOffset + inAdvance && ad.className.indexOf("loaded") < 0) {
 	            if(ad.id == "adsense_submenu"){
-	            	add_ad("7686901251","","link",ad.id,"","Guiapaqueteria US Guias Abajo H1 Responsive");
+	            	add_ad("7686901251","","link",ad.id,"","Guiapaqueteria US Guias Abajo 1er H2 Responsive");
 	            	ad.classList.add('loaded');
 	            }else if(ad.id == "adsense_1erh2"){
 	            	add_ad("2657935857","","auto",ad.id,"","Guiapaqueteria US Guias Abajo 1er H2 Responsive");
